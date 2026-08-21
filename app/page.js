2e66391fe27cb8f204196f350417d5d4691ab5e2
Alec Maanavi <amaanavi84@gmail.com>
2026-08-21 12:26:21 -0400

    Add centered FIJI emblem over purple background

--- app/page.js
+++ app/page.js
@@ -4,8 +4,21 @@ export default function Home() {
       style={{
         minHeight: "100vh",
         width: "100%",
-        backgroundColor: "#4B286D",
+        backgroundColor: "#6C3BAA",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
-    />
+    >
+      <img
+        src="/IMG_4307.jpeg"
+        alt="Emblem"
+        style={{
+          width: "min(40vh, 80vw)",
+          height: "auto",
+          borderRadius: "50%",
+        }}
+      />
+    </main>
   );
 }
